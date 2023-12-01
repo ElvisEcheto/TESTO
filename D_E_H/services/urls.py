3bb5e3ed917cbc349c883edb8825bb71e541e339
SebastianGR1 <granadasebastian140@gmail.com>
2023-12-01 11:30:07 -0500

    Alo

--- D_E_H/services/urls.py
+++ D_E_H/services/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [      
-    path('', views.services, name='services'),            
+    path('', views.services, name='services'),  
+    path('services_status_/<int:services_ID>/', views.change_status_services, name='service_status'),          
 ]
--- D_E_H/services/views.py
+++ D_E_H/services/views.py
@@ -6,8 +6,8 @@ def services(request):
     services_list = Services.objects.all()    
     return render(request, 'services/index.html', {'services_list': services_list})
 
-def change_status_costumers(request, costumer_id):
-    costumer = Costumer.objects.get(pk=costumer_id)
-    costumer.status = not costumer.status
-    costumer.save()
-    return redirect('costumers')
+def change_status_services(request, service_ID):
+    service = Services.objects.get(pk=service_ID)
+    service.status = not service.status
+    service.save()
+    return redirect('services')
