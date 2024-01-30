957d300512392eeae145cdbdd09ab1e496582671
kevinsan4 <kevinsanchez.futbol@gmail.com>
2024-01-30 11:22:07 -0500

    jf

--- ORO/proyecto_f/typedocuments/urls.py
+++ ORO/proyecto_f/typedocuments/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [      
     path('', views.typedocuments, name='typedocuments'),
 	path('typedocument_status_/<int:typedocument_id>/', views.change_status_typedocument, name='typedocument_status'),
-    path('create/', views.create_typedocument, name='create_typedocument'),          
+    path('create/', views.create_typedocument, name='create_typedocument'),        
+    path('detail/<int:typedocument_id>/', views.detail_typedocument, name='detail_typedocument'),  
+    path('delete/<int:typedocument_id>/', views.delete_typedocument, name='delete_typedocument'),
 ]
--- ORO/proyecto_f/typedocuments/views.py
+++ ORO/proyecto_f/typedocuments/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 
 from typedocuments.models import Typedocument
 
+from django.http import JsonResponse
+
+from django.contrib import messages
+
 from .forms import TypedocumentForm
 
 def create_typedocument(request):
@@ -19,4 +23,19 @@ def change_status_typedocument(request, typedocument_id):
     typedocument = Typedocument.objects.get(pk=typedocument_id)
     typedocument.status = not typedocument.status
     typedocument.save()
+    return redirect('typedocuments')
+
+def detail_typedocument(request, typedocument_id):
+    typedocument = Typedocument.objects.get(pk=typedocument_id)
+    data = { 'name': typedocument.name, 'acronyms' : typedocument.acronyms }    
+    return JsonResponse(data)
+
+
+def delete_typedocument(request, typedocument_id):
+    typedocument = Typedocument.objects.get(pk=typedocument_id)
+    try:
+        typedocument.delete()        
+        messages.success(request, 'tipo documento eliminado correctamente.')
+    except:
+        messages.error(request, 'No se puede eliminar el autor porque está asociado a un libro.')
     return redirect('typedocuments')
