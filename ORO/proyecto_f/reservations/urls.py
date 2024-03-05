34cb190f1c4a8a57f6437e3e824925d9bad7249c
ElvisEcheto <elvisechetob@benjaminherreramedellin.edu.co>
2024-03-05 00:04:29 -0500

    s

--- ORO/proyecto_f/reservations/urls.py
+++ ORO/proyecto_f/reservations/urls.py
@@ -1,9 +1,12 @@
 from . import views
 from django.urls import path
+from django.urls import path
+from .views import ReservationsPDFView
 
 urlpatterns = [      
     path('', views.reservations, name='reservations'),
     path('create/', views.create_reservation, name='create_reservation'),
     path('detail/<int:reservation_id>/', views.detail_reservation, name='detail_reservation'),   
-    path('edit/<int:reservation_id>/', views.edit_reservation, name='edit_reservation'), 
+    path('edit/<int:reservation_id>/', views.edit_reservation, name='edit_reservation'),
+    path('reservas-pdf/', ReservationsPDFView.as_view(), name='reservas_pdf'),
 ]
